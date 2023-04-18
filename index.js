c76b19d38dae028074ff19279ddf1ba093483728
Matija2003 <matijaj@viken.no>
2023-04-18 13:17:13 +0200

    Box

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const firkant = document.querySelector('.firkant');
-  let posX = 0;
-  let posY = 0;
+/*function for å beveg firekant med w,s,a,d*/
 
-  document.addEventListener('keydown', function (event) {
-    switch (event.key) {
-      case 'w':
-      case 'ArrowUp':
-        posY -= 10;
-        break;
-      case 'a':
-      case 'ArrowLeft':
-        posX -= 10;
-        break;
-      case 's':
-      case 'ArrowDown':
-        posY += 10;
-        break;
-      case 'd':
-      case 'ArrowRight':
-        posX += 10;
-        break;
-    }
-    firkant.style.top = posY + 'px';
-    firkant.style.left = posX + 'px';
-  });
+document.addEventListener('keydown', function (event) {
+  let rect1 = firkant1.getBoundingClientRect();
+  let maxX = window.innerWidth - firkant1.offsetWidth;
+  let maxY = window.innerHeight - firkant1.offsetHeight;
+
+  if (event.key == 'w') {
+    firkant1.style.top = Math.max(rect1.top - 10, 0) + 'px';
+  } else if (event.key == 's') {
+    firkant1.style.top = Math.min(rect1.top + 10, maxY) + 'px';
+  } else if (event.key == 'a') {
+    firkant1.style.left = Math.max(rect1.left - 10, 0) + 'px';
+  } else if (event.key == 'd') {
+    firkant1.style.left = Math.min(rect1.left + 10, maxX) + 'px';
+  }
+
+  // Sjekk om firkant1 går utenfor skjermen, og juster hvis nødvendig
+  rect1 = firkant1.getBoundingClientRect();
+  if (rect1.left < 0) {
+    firkant1.style.left = '0px';
+  } else if (rect1.right > window.innerWidth) {
+    firkant1.style.left = window.innerWidth - firkant1.offsetWidth + 'px';
+  }
+  if (rect1.top < 0) {
+    firkant1.style.top = '0px';
+  } else if (rect1.bottom > window.innerHeight) {
+    firkant1.style.top = window.innerHeight - firkant1.offsetHeight + 'px';
+  }
 });
